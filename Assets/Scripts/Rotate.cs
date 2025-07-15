b57247ba62ea31512db56f53899db8faa00a8fb3
DEMONCRXSS <156143511+DEMONCRXSS@users.noreply.github.com>
2025-07-14 20:51:29 -0600

    coins

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+
+    private Vector3 rotationSpeed = new Vector3 (0, 100, 0);
+
+    private void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime, Space.Self);
+    }
+
+}
+
